82a142c8f53b38da9a13f345ec5ecbef357aa644
hitman475 <31922899+hitman475@users.noreply.github.com>
2020-04-09 16:15:14 +0800

    Create Camera.h

---
+++ resource/other/Camera.h
@@ -0,0 +1,137 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <vector>
+
+// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+enum Camera_Movement {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT,
+	DOWN,
+	UP
+};
+
+// Default camera values
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 2.5f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM = 45.0f;
+
+
+// An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
+class Camera
+{
+public:
+	// Camera Attributes
+	glm::vec3 Position;		//camera位置
+	glm::vec3 Front;		//camera的正前方方向向量
+	glm::vec3 Up;			//camera的上轴
+	glm::vec3 Right;		//camera的右轴
+	glm::vec3 WorldUp;		//世界坐标的向上的向量
+	// 欧拉角
+	float Yaw;				//偏航角 横向
+	float Pitch;			//俯仰角
+	// Camera options		
+	float MovementSpeed;	//键盘移动速度
+	float MouseSensitivity;	//鼠标灵活度
+	float Zoom;				//透视投影的视野 值越大 距离目标越远 主要是用来控制滚轮缩放
+
+	// 使用向量来构造camera
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		Position = position;
+		WorldUp = up;
+		Yaw = yaw;
+		Pitch = pitch;
+		updateCameraVectors();
+	}
+	// 使用欧拉角构建camera
+	Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		Position = glm::vec3(posX, posY, posZ);
+		WorldUp = glm::vec3(upX, upY, upZ);
+		Yaw = yaw;
+		Pitch = pitch;
+		updateCameraVectors();
+	}
+
+
+	//使用欧拉角来创建lookAt矩阵
+	glm::mat4 GetViewMatrix()
+	{
+		//para1:为camera的位置 para2:为camera指向的方向向量 para3:为世界上方向量
+		return glm::lookAt(Position, Position + Front, Up);
+	}
+
+	//处理WSADQE的键盘输入 改变方向
+	void ProcessKeyboard(Camera_Movement direction, float deltaTime)
+	{
+		float velocity = MovementSpeed * deltaTime;
+		if (direction == FORWARD)
+			Position += Front * velocity;
+		if (direction == BACKWARD)
+			Position -= Front * velocity;
+		if (direction == LEFT)
+			Position -= Right * velocity;
+		if (direction == RIGHT)
+			Position += Right * velocity;
+		if (direction == UP)
+			Position += Up * velocity;
+		if (direction == DOWN)
+			Position -= Up * velocity;
+	}
+
+	// para1：鼠标x轴便宜量 para2:鼠标y轴偏移量 para3：判断俯仰角是否越界
+	void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
+	{
+		xoffset *= MouseSensitivity;
+		yoffset *= MouseSensitivity;
+
+		Yaw += xoffset;
+		Pitch += yoffset;
+
+		if (constrainPitch)
+		{
+			if (Pitch > 89.0f)
+				Pitch = 89.0f;
+			if (Pitch < -89.0f)
+				Pitch = -89.0f;
+		}
+
+		updateCameraVectors();
+	}
+
+	// 处理鼠标滚轮的输入
+	void ProcessMouseScroll(float yoffset)
+	{
+		if (Zoom >= 1.0f && Zoom <= 45.0f)
+			Zoom -= yoffset;
+		if (Zoom <= 1.0f)
+			Zoom = 1.0f;
+		if (Zoom >= 45.0f)
+			Zoom = 45.0f;
+	}
+
+private:
+	
+	void updateCameraVectors()
+	{
+		//计算欧拉角来更新鼠标转动的角度
+		glm::vec3 front;
+		front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		front.y = sin(glm::radians(Pitch));
+		front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		Front = glm::normalize(front);
+		// 重新就是上轴和右轴
+		Right = glm::normalize(glm::cross(Front, WorldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+		Up = glm::normalize(glm::cross(Right, Front));
+	}
+};
+#endif
